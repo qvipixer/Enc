c2c71b11beec782766ead5cf318daaeebe50f62d
SerggTech <qvipixer@gmail.com>
2023-12-11 01:36:25 +0300

    !

--- Enc_Electro_Site/main/views.py
+++ Enc_Electro_Site/main/views.py
@@ -5,39 +5,42 @@ from .models import Myfiles
 
 # Create your views here.
 def page(request):
-    """
-    all_files = Myfiles.objects.all()
-    context = {
-        'all_files': all_files
-    }
-
-    if request.POST:
-        # print(request.POST.get('text'))
-        # print(request.FILES.get('file'))
-        Myfiles.objects.create(
-            text=request.POST.get('text'),
-            file=request.FILES.get('file')
-        )
-    """
-    return render(request, 'index.html')
+    # return render(request, 'index.html')
+    return render(
+        request,
+        template_name="main/index.html",
+        context={"title": "Главная страница"},
+    )
 
 
 def download(request):
+    """
     all_files = Myfiles.objects.all()
     context = {
         'all_files': all_files
     }
 
     return render(request, 'download.html', context)
-    pass
+    """
+
+    all_files = Myfiles.objects.all()
+    return render(
+        request,
+        template_name="main/download.html",
+        context={"all_files": all_files},
+    )
 
 
 def upload(request):
     # print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     # print(request.GET)
-    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    print(request.META.get('REMOTE_ADDR'))
-    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    print(
+        "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    )
+    print(request.META.get("REMOTE_ADDR"))
+    print(
+        "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    )
     # print(request.POST)
     # print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     # print(request.FILES)
@@ -49,7 +52,7 @@ def upload(request):
     # print(request.FILES.get('file'))
     if request.GET:
         print("!!! __GET__ !!!")
-        print(request.META.get('REMOTE_ADDR'))
+        print(request.META.get("REMOTE_ADDR"))
         print(request.GET)
         print(request.POST)
         print(request.FILES)
@@ -59,7 +62,7 @@ def upload(request):
         print("!!! __POST__ !!!")
         # print(request)
         # print(request.__dict__)
-        print(request.META.get('REMOTE_ADDR'))
+        print(request.META.get("REMOTE_ADDR"))
         print(request.POST)
         print(request.FILES)
         # print(request.POST.get('text'))
@@ -68,10 +71,15 @@ def upload(request):
         print("!!! __POST__ !!!")
 
         Myfiles.objects.create(
-            text=request.POST.get('text'),
-            text_to=request.POST.get('text_to'),
-            file=request.FILES.get('file'),
-            remote_ip=request.META.get('REMOTE_ADDR')
+            text=request.POST.get("text"),
+            text_to=request.POST.get("text_to"),
+            file=request.FILES.get("file"),
+            remote_ip=request.META.get("REMOTE_ADDR"),
         )
 
-    return render(request, 'upload.html')
+    # return render(request, 'upload.html')
+    return render(
+        request,
+        template_name="main/upload.html",
+        context={"title": "Загрузка файлов"},
+    )
