967cd604cecd509fc3e6d088d404ab4d3eb0644b
SerggTech <qvipixer@gmail.com>
2024-02-09 14:27:49 +0300

    end week on work

--- Enc_Electro_Site/main/urls.py
+++ Enc_Electro_Site/main/urls.py
@@ -8,8 +8,8 @@ from .views import user_logout
 
 urlpatterns = [
     path("", page, name="main_page"),
-    path("upload", file_upload, name="upload"),
-    path("download", file_download, name="download"),
+    path("file/upload/", file_upload, name="upload"),
+    path("file/download/", file_download, name="download"),
     path("login/", user_login, name="login"),
     path("logout/", user_logout, name="logout"),
 ]
