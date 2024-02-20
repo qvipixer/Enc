e3829faf7c371d1072be670d4c0464871572ebd2
SerggTech <qvipixer@gmail.com>
2024-02-20 08:04:10 +0300

    fix work

--- Enc_Electro_Site/Enc_Electro_Site/settings.py
+++ Enc_Electro_Site/Enc_Electro_Site/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "bootstrap5",
+    # "bootstrap5",
     "main",
     "as",
     "Enc_object",
