97db4fded4aa7ff1d1ac790b0cfe823edda2e5a7
SerggTech <qvipixer@gmail.com>
2024-02-28 16:24:33 +0300

    fix 28.02.2024

--- Enc_Electro_Site/elc/migrations/0001_initial.py
+++ Enc_Electro_Site/elc/migrations/0001_initial.py
@@ -5,29 +5,77 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
     dependencies = [
-        ('Enc_object', '0001_initial'),
+        ("Enc_object", "0001_initial"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='ElcLog',
+            name="ElcLog",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('record_text_title', models.CharField(max_length=100, verbose_name='Заголовок записи')),
-                ('record_text_full', models.TextField(max_length=9999, verbose_name='Текст записи')),
-                ('record_author', models.CharField(max_length=100, verbose_name='Автор записи')),
-                ('record_data_create', models.DateTimeField(auto_now=True, verbose_name='Дата создания записи')),
-                ('record_electrical_room', models.ForeignKey(blank=True, on_delete=django.db.models.deletion.CASCADE, related_name='elc_log_electrical_room', to='Enc_object.encelectricalroom', verbose_name='Электропомещение')),
-                ('record_mechanism', models.ForeignKey(blank=True, on_delete=django.db.models.deletion.CASCADE, related_name='elc_log_mechanism', to='Enc_object.encmechanism', verbose_name='Механизм')),
-                ('record_object', models.ForeignKey(blank=True, on_delete=django.db.models.deletion.CASCADE, related_name='elc_log_object', to='Enc_object.encobject', verbose_name='Объект')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "record_text_title",
+                    models.CharField(max_length=100, verbose_name="Заголовок записи"),
+                ),
+                (
+                    "record_text_full",
+                    models.TextField(max_length=9999, verbose_name="Текст записи"),
+                ),
+                (
+                    "record_author",
+                    models.CharField(max_length=100, verbose_name="Автор записи"),
+                ),
+                (
+                    "record_data_create",
+                    models.DateTimeField(
+                        auto_now=True, verbose_name="Дата создания записи"
+                    ),
+                ),
+                (
+                    "record_electrical_room",
+                    models.ForeignKey(
+                        blank=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="elc_log_electrical_room",
+                        to="Enc_object.encelectricalroom",
+                        verbose_name="Электропомещение",
+                    ),
+                ),
+                (
+                    "record_mechanism",
+                    models.ForeignKey(
+                        blank=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="elc_log_mechanism",
+                        to="Enc_object.encmechanism",
+                        verbose_name="Механизм",
+                    ),
+                ),
+                (
+                    "record_object",
+                    models.ForeignKey(
+                        blank=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="elc_log_object",
+                        to="Enc_object.encobject",
+                        verbose_name="Объект",
+                    ),
+                ),
             ],
             options={
-                'verbose_name': 'Запись Элекриков',
-                'verbose_name_plural': 'Записи Элекриков',
+                "verbose_name": "Запись Элекриков",
+                "verbose_name_plural": "Записи Элекриков",
             },
         ),
     ]
