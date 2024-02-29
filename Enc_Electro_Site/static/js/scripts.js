378d7ef09da40d129a0ddab502be0e66127f80d3
serggtech-home <serggtech@gmail.com>
2024-02-29 23:53:05 +0300

    29.02.2024 fome fix

--- Enc_Electro_Site/static/js/scripts.js
+++ Enc_Electro_Site/static/js/scripts.js
@@ -1,11 +1,11 @@
 function SearchTag(id){
-var step_a = document.getElementById(id).innerHTML; // получил данные
-var step_b = step_a.replace(/&lt;FC&gt;/g, '<a href="{% url 'as_search_results' %}?search=<FC>'); // нашел тэг и поменял
-var step_c = step_b.replace(/&lt;\/FC&gt;/g, '">FC<\/a>'); // записал новые данные
+    var step_a = document.getElementById(id).innerHTML; // получил данные
+    var step_b = step_a.replace(/&lt;FC&gt;/g, '<a href="{% url 'as_search_results' %}?search=<FC>'); // нашел тэг и поменял
+    var step_c = step_b.replace(/&lt;\/FC&gt;/g, '">FC<\/a>'); // записал новые данные
 
-document.getElementById(id).innerHTML = step_c;
+    document.getElementById(id).innerHTML = step_c;
 
-console.log(step_a);
-console.log(step_b);
-console.log(step_c);
+    console.log(step_a);
+    console.log(step_b);
+    console.log(step_c);
 }
